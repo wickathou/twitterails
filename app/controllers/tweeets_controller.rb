792b43703eeb84d36b50efaa7baccae087f75f9c
wickathou <javidx@gmail.com>
2020-07-25 18:54:27 +0200

    Added: Model dependency when user profile is deleted, now dependent tweeets get deleted as well Added who to follow dynamics

--- app/controllers/tweeets_controller.rb
+++ app/controllers/tweeets_controller.rb
@@ -5,6 +5,7 @@ class TweeetsController < ApplicationController
   # GET /tweeets
   # GET /tweeets.json
   def index
+    @users = User.all
     @tweeets = Tweeet.all.order('created_at DESC')
     @tweeet = Tweeet.new
   end
